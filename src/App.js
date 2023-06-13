40f09794d7ef4995c64ab774589b52c06c32ed87
ehimze007 <ehimze007@gmail.com>
2023-06-13 20:42:45 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Navbar, Container, Card } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#" className="App-link">
+            My React App
+          </Navbar.Brand>
+        </Navbar>
+        <Container>
+          <h1>My React App</h1>
+          <Card>
+            <Card.Body>
+              <Card.Title>My React App1</Card.Title>
+              <Card.Text>Content.</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>My React App App2</Card.Title>
+              <Card.Text>Content.</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>My React App App3</Card.Title>
+              <Card.Text>Content.</Card.Text>
+            </Card.Body>
+          </Card>
+        </Container>
+      </div>
+    </>
   );
 }
 
